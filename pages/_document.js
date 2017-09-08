971a986bf3e1388977a73da1125bb7403d7b4b15
Rikard Eide <rix1@otovo.no>
2017-09-08 09:37:23 +0200

    Base: Added some extra styles, fonts and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,21 +9,26 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
+          <title>NeoResQ: UMP-123</title>
           {styleTags}
+          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css"
-          />
           <link rel="stylesheet" href="https://unpkg.com/tachyons@4.7.0/css/tachyons.min.css" />
-          <link href="https://fonts.googleapis.com/css?family=Lato|Lobster" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:400,900"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <style>{`
             body {
               font-size: 16px;
-              background-color: papayawhip;
+              font-family: Lato, sans-serif;
+            }
+
+            p {
+              line-height: 1.5;
+              color: #434A54;
             }
 
             .ff-lobster {
@@ -41,6 +46,34 @@ export default class MyDocument extends Document {
             }
             .shadow1 {
               box-shadow: 0 8px 34px 0 rgba(0, 0, 0, 0.3);
+            }
+
+            .ts {
+              text-shadow: 0 1px 2px rgba(0,0,0,0.2);
+            }
+
+            .flex-center {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+
+            .gray {
+              color: #434A54;
+            }
+            .red {
+              color: #da4a4a;
+            }
+            .red-dark {
+              color: #7c2c2c;
+            }
+
+            .bg-gray-light {
+              background-color: #F7F8FA;
+            }
+
+            .shadow-inset {
+              box-shadow: inset 0px 0px 10px -1px rgba(0,0,0, 0.2);
             }
           `}</style>
           <div className="root">{main}</div>
