78d4b8bf97116a76443ca8ed7a360cd7bbe6fbe8
Rikard Eide <rix1@otovo.no>
2017-09-08 09:43:21 +0200

    Added all components to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-import NavigationButtons from '../components/NavigationButtons';
-import Paper from '../components/Paper';
+
 import Navbar from '../components/Navbar';
+import BGImage from '../components/BGImage';
+import ProductSection from '../components/ProductSection';
+import TeamSection from '../components/TeamSection';
+import ContactSection from '../components/ContactSection';
 
 const MainBody = styled.div`
   width: 100vw;
   height: 100vh;
   position: relative;
-  display: flex;
-  flex-direction: column;
 `;
-const Topbar = styled.div`
-  width: 100%;
-  height: 5px;
-  background-color: red;
-`;
+
+const Redline = () => <div className="w-100 bg-red absolute" style={{ height: '4px' }} />;
 
 export default () => (
   <MainBody>
-    <Topbar />
+    <Redline />
     <Navbar />
-    <h1 className="ff-lobster">Hallo, verden!</h1>
-    <div></div>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <NavigationButtons />
+    <BGImage filePath="/static/bg-jesus.jpg">
+      <div className="w-100 h-100 flex-center">
+        <div className="tc ph4">
+          <h1 className="f-subheadline-l f1-m f2 white ff-lato ts">
+            <span className="db f3-ns f4 ttu tracked white o-70 ts-white">We believe:</span>
+            All lives are equal
+          </h1>
+          <p className="ff-lato white f3-ns f4 mw7 center ts">
+            – But nature doesn't always agree and some infants need a little extra just to get
+            started. Saving those unlucky few shouldn't be reserved to experts with 20-years
+            experience with <span className="">neonates and intensive care.</span>
+          </p>
+          <p className="ff-lato white f3-ns f4 mw7 center ts">
+            That's why we designed the <strong>UMP-123</strong>{' '}
+          </p>
+        </div>
+      </div>
+    </BGImage>
+
+    <ProductSection />
+
+    <TeamSection />
+
+    <ContactSection />
+    <Redline />
   </MainBody>
 );
